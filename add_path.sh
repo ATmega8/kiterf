3609a9f380726d0bec0faa48afb8cb93e0f04221
xiongyu <xiongyu@espressif.com>
2019-05-13 15:39:56 +0800

    feature(cmake): add cmake 3.14.2 path

--- add_path.sh
+++ add_path.sh
@@ -9,3 +9,4 @@ export K210_KFLASH_PATH=$TOOL_PATH/K210/kflash
 export LD_LIBRARY_PATH=$K210_TOOLCHAIN_PATH:$LD_LIBRARY_PATH
 export PATH=$TOOL_PATH/ESP32/xtensa-esp32-elf/bin:$TOOL_PATH/ESP32/esp-idf/tools:$PATH
 export IDF_PATH=$TOOL_PATH/ESP32/esp-idf
+export PATH=$KITERF_PATH/tools/cmake-3.14.2/bin:$PATH
